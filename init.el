5f25c3c170a7b7902acff37885b799d6810d2cb6
Vince <lee.wenzhu@gmail.com>
2014-11-25 05:16:58 +0000

    use column-mode, yas-global-mode, auto-complete-mode as default.

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 (global-set-key (kbd "C-c a") 'auto-complete-mode)
 (global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c =") 'er/expand-region)
+(global-set-key (kbd "C-c C-l") 'goto-line)
 
 ;; add association of .m file with octave-mode
 (setq auto-mode-alist
@@ -20,6 +21,21 @@
 ;; disable the backup files end with ~
 (setq make-backup-files nil)
 
+;; set column mode default
+(setq column-number-mode t)
+
+;; set yas-global-mode default
+(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-20141117.327")
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; set auto-complete-mode default
+(add-to-list 'load-path "~/.emacs.d/elpa/popup-20141120.1742")
+(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20141111.2346")
+(require 'auto-complete)
+(global-auto-complete-mode 1)
+
+;; use melpa package repository
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
