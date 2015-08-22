3cbc1bc0e1bb2a67d2cae4f1814ef51c6a5fb0d6
Vince <lee.wenzhu@gmail.com>
2015-08-22 15:55:02 +0800

    update elpa source, set c-c c-w binding to whitespace-mode.

--- init.el
+++ init.el
@@ -5,3 +5,17 @@
 (setq-default tab-always-indent t)
 (setq-default indent-tabs-mode t)
 (setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(global-set-key (kbd "C-c C-w") 'whitespace-mode)
+
+(require 'package)
+(add-to-list 'package-archives
+			 '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
+
